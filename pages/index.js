3ebea0ff9ab4805326aebb6d8948f3f04be64ce0
senaarth <senarth@gmail.com>
2021-08-05 17:39:56 -0300

    styles posts list

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { FiUser, FiCalendar } from 'react-icons/fi';
+
+import styles from './home.module.css';
 
 export default function Home() {
   return (
     <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100%',
-        width: '100%',
-      }}
+      className={styles.container}
     >
       <Head>
         <title>MyTrip Blog</title>
       </Head>
+      <div className={styles.contentContainer}>
+        <Link href={`/post/id`}>
+          <a
+            className={styles.postContainer}
+          >
+            <h1 className={styles.title}>Título do Post</h1>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+            >
+              <FiCalendar size={15} color="#A9AFB2" />
+              <p className={styles.infoText}>
+                23 Jul 2021
+              </p>
+            </div>
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
